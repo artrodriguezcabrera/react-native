ee9e6b05034a2c76eea9c5c2d462ac8b752d1a5c
Arturo Rodriguez <fastmode@gmail.com>
2019-06-09 13:08:52 -0400

    added timing and statusbar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, SafeAreaView, TouchableOpacity, Animated, Easing } from 'react-native';
+import { ScrollView, SafeAreaView, TouchableOpacity, Animated, Easing, StatusBar } from 'react-native';
 import styled from 'styled-components';
 import Card from '../components/Card';
 import { Icon } from 'expo';
@@ -24,24 +24,41 @@ function mapDispatchToProps(dispatch) {
 class HomeScreen extends React.Component {
   state = {
     scale: new Animated.Value(1),
-    opacity: new Animated.Value(1);
+    opacity: new Animated.Value(1)
   };
 
+  componentDidMount() {
+    StatusBar.setBarStyle("dark-content", true);
+  }
   componentDidUpdate() {
     this.toggleMenu()
   }
 
   toggleMenu = () => {
     if (this.props.action == "openMenu") {
-      Animated.spring(this.state.scale, {
-        toValue: 0.9
+      Animated.timing(this.state.scale, {
+        toValue: 0.9,
+        duration: 300,
+        easing: Easing.in()
       }).start();
+      Animated.spring(this.state.opacity, {
+        toValue: 0.5
+      }).start();
+
+      StatusBar.setBarStyle("light-content", true);
     }
 
     if (this.props.action == "closeMenu") {
-      Animated.spring(this.state.scale, {
+      Animated.timing(this.state.scale, {
+        toValue: 1,
+        duration: 300,
+        easing: Easing.in()
+      }).start();
+      Animated.spring(this.state.opacity, {
         toValue: 1
       }).start();
+
+      StatusBar.setBarStyle("dark-content", true);
     }
   };
 
