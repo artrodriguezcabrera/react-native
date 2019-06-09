3b1f93059a35c5663ecf93e4a3e7970cc1d03176
Arturo Rodriguez <fastmode@gmail.com>
2019-06-09 13:00:57 -0400

    add Redux and Animate states for Menu and HomeScreens

--- App.js
+++ App.js
@@ -1,160 +1,29 @@
 import React from 'react';
-import { ScrollView, SafeAreaView } from 'react-native';
-import styled from 'styled-components';
-import Card from './components/Card';
-import { Icon } from 'expo';
-import { NotificationIcon } from "./components/Icons";
-import Logo from './components/Logo';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Container>
-        <SafeAreaView>
-          <ScrollView>
-            <TitleBar>
-              <Avatar source={require('./assets/avatar.jpg')}/>
-              <Title>Welcome back,</Title>
-              <Name>Arturo</Name>
-              <NotificationIcon 
-                style={{ position: "absolute", right: 20, top: 5 }}
-              />
-            </TitleBar>
-            <ScrollView 
-              style={{ 
-                flexDirection: "row", 
-                padding: 20, 
-                paddingLeft: 12, 
-                paddingTop: 30 
-              }} 
-              horizontal={true}
-              showsHorizontalScrollIndicator={false} 
-            >
-              {logos.map((logo, index) => (
-                <Logo key={index} image={logo.image} text={logo.text} />
-              ))}
-            </ScrollView>
-            <Subtitle>Find awesome wallpaper</Subtitle>
-            <ScrollView 
-              horizontal={true} 
-              style={{ paddingBottom: 30 }} 
-              showsHorizontalScrollIndicator={false}
-            >
-              {cards.map((card, index) => (
-                <Card 
-                  key={index}
-                  title={card.title} 
-                  image={card.image}
-                  caption={card.caption}
-                  logo={card.logo}
-                  subtitle={card.subtitle}
-                />
-              ))}
-            </ScrollView>
-          </ScrollView>
-        </SafeAreaView>
-      </Container>
-    );
-  }
+const initialState = {
+  action: ""
 }
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #f0f3f5;
-`;
-
-const Avatar = styled.Image`
-  width: 44px;
-  height: 44px;
-  background: black;
-  border-radius: 22px;
-  margin-left: 20px;
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
-const Title = styled.Text`
-  font-size: 16px;
-  color: #b8bece;
-  font-weight: 500;
-`;
-
-const Name = styled.Text`
-  font-size: 20px;
-  color: #3c4560;
-  font-weight: bold;
-`;
-
-const TitleBar = styled.View`
-  width: 100%;
-  margin-top: 50px;
-  padding-left: 80px;
-`;
-
-const Subtitle = styled.Text`
-  color: #b8bece;
-  font-weight: 600;
-  font-size: 15px;
-  margin-left: 20px;
-  margin-top: 20px;
-  text-transform: uppercase;
-`;
-
-
-const logos = [
-  {
-    image: require("./assets/logo-framerx.png"),
-    text: "Framer X"
-  },
-  {
-    image: require("./assets/logo-figma.png"),
-    text: "Figma"
-  },
-  {
-    image: require("./assets/logo-studio.png"),
-    text: "Studio"
-  },
-  {
-    image: require("./assets/logo-react.png"),
-    text: "React"
-  },
-  {
-    image: require("./assets/logo-swift.png"),
-    text: "Swift"
-  },
-  {
-    image: require("./assets/logo-sketch.png"),
-    text: "Sketch"
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU": 
+      return { action: "openMenu"};
+    case "CLOSE_MENU":
+      return { action: "closeMenu"};
+    default:
+      return state;
   }
-];
+};
 
-const cards = [
-  {
-    title: "React Native for Designers",
-    image: require("./assets/background11.jpg"),
-    subtitle: "React Native",
-    caption: "1 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  },
-  {
-    title: "Styled Components",
-    image: require("./assets/background12.jpg"),
-    subtitle: "React Native",
-    caption: "2 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  },
-  {
-    title: "Props and Icons",
-    image: require("./assets/background13.jpg"),
-    subtitle: "React Native",
-    caption: "3 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  },
-  {
-    title: "Static Data and Loop",
-    image: require("./assets/background14.jpg"),
-    subtitle: "React Native",
-    caption: "4 of 12 sections",
-    logo: require("./assets/logo-react.png")
-  }
-];
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
+)
+
+export default App;
