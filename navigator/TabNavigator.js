b5872a3e3fd628387e21b29e2c96aea41da7485a
Arturo Rodriguez <fastmode@gmail.com>
2019-06-10 18:22:45 -0400

    create TabNavigator

---
+++ navigator/TabNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import HomeScreen from '../screens/HomeScreen';
+import SectionScreen from '../screens/SectionScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Section: SectionScreen
+});
+
+const CoursesStack = createStackNavigator({
+  Courses: SectionScreen
+});
+
+const ProjectsStack = createStackNavigator({
+  Projects: SectionScreen
+})
+
+const TabNavigator = createBottomTabNavigator({
+  HomeStack, 
+  CoursesStack, 
+  ProjectsStack
+});
