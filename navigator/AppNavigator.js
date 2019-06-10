0c7e9dce8eba3591cee999816a077e8191fa75a8
Arturo Rodriguez <fastmode@gmail.com>
2019-06-10 19:05:46 -0400

    Add AppNavigator and TabNavigator

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -1,6 +1,7 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from '../screens/HomeScreen';
 import SectionScreen from '../screens/SectionScreen';
+import TabNavigator from './TabNavigator';
 
 const AppNavigator = createStackNavigator({
   Home: HomeScreen,
@@ -9,4 +10,4 @@ const AppNavigator = createStackNavigator({
   mode: "modal"
 });
 
-export default createAppContainer(AppNavigator);
+export default createAppContainer(TabNavigator);
